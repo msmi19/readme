3a3f75d10bf393785d6a3757cd9f5ff3c3bccce7
Morgan Smith <morgansmith0219@gmail.com>
2019-07-10 21:25:24 +0000

    form submission created and ready to use

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,15 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    Book.create(book_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :description, :author)
+  end
 end
