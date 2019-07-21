f3c26d14ddbee628902a67a5e2fab793444c61d6
Morgan Smith <morgansmith0219@gmail.com>
2019-07-21 19:00:43 +0000

    Added user email to the book created on index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index 
     @books = Book.paginate(page: params[:page])
   end
@@ -8,7 +10,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    Book.create(book_params)
+    current_user.books.create(book_params)
     redirect_to root_path
   end
 
